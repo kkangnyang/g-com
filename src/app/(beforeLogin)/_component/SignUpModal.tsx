afdc159821b1dcf84e2128c4489b171c4ef94c7a
김가영 <kimgayoung957@gmail.com>
2024-07-25 18:27:05 +0900

    sighup api add

--- src/app/(beforeLogin)/_component/SignUpModal.tsx
+++ src/app/(beforeLogin)/_component/SignUpModal.tsx
@@ -1,74 +1,70 @@
 "use client";
 
-import { useState } from "react";
 import style from './signup.module.css';
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import onSubmit from '../_lib/signup';
+import { useFormState, useFormStatus } from 'react-dom';
+
+function showMessage(messasge: string | null | undefined) {
+  if (messasge === 'no_id') {
+    return '아이디를 입력하세요.';
+  }
+  if (messasge === 'no_name') {
+    return '닉네임을 입력하세요.';
+  }
+  if (messasge === 'no_password') {
+    return '비밀번호를 입력하세요.';
+  }
+  if (messasge === 'no_image') {
+    return '이미지를 업로드하세요.';
+  }
+  if (messasge === 'user_exists') {
+    return '이미 사용 중인 아이디입니다.';
+  }
+  return '';
+}
 
 export default function SignUpModal() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickname, setNickName] = useState('');
-  const [image, setImage] = useState('');
-  const [imageFile, setImageFile] = useState('');
-
-//   const router = useRouter();
-  const onClickClose = () => {
-    // router.back();
-  }
-
-  const onChangeId = () => {};
-  const onChangeNickname = () => {};
-  const onChangePassword = () => {};
-  const onChangeImageFile = () => {};
-
-
+  const [state, formAction] = useFormState(onSubmit, { message: '' });
+  const { pending } = useFormStatus();
   return (
     <>
       <div className={style.modalBackground}>
         <div className={style.modal}>
           <div className={style.modalHeader}>
-            <button className={style.closeButton} onClick={onClickClose}>
-              <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
-                   className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
-                <g>
-                  <path
-                    d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                </g>
-              </svg>
-            </button>
+            <BackButton />
             <div>계정을 생성하세요.</div>
           </div>
-          <form>
+          <form action={formAction}>
             <div className={style.modalBody}>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="id">아이디</label>
-                <input id="id" className={style.input} type="text" placeholder=""
-                       value={id}
-                       onChange={onChangeId}
+                <input id="id" name="id" className={style.input} type="text" placeholder=""
+                  required
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="name">닉네임</label>
-                <input id="name" className={style.input} type="text" placeholder=""
-                       value={nickname}
-                       onChange={onChangeNickname}
+                <input id="name" name="name" className={style.input} type="text" placeholder=""
+                  required
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="password">비밀번호</label>
-                <input id="password" className={style.input} type="password" placeholder=""
-                       value={password}
-                       onChange={onChangePassword}
+                <input id="password" name="password" className={style.input} type="password" placeholder=""
+                  required
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="image">프로필</label>
-                <input id="image" className={style.input} type="file" accept="image/*"
-                       onChange={onChangeImageFile}
+                <input id="image" name="image" className={style.input} type="file" accept="image/*"
+                  required
                 />
               </div>
             </div>
             <div className={style.modalFooter}>
-              <button className={style.actionButton} disabled>가입하기</button>
+              <button className={style.actionButton} disabled={pending}>가입하기</button>
+              <div className={style.error}>{showMessage(state?.message)}</div>
             </div>
           </form>
         </div>
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,35 @@
+import { log } from 'console';
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+    http.post('/api/login', () => {
+        return HttpResponse.json({
+            userId: 1,
+            nickname: '가영',
+            id: 'gayoung',
+            image: '/glogo.png'
+        }, {
+            headers: {
+                'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+            }
+        })
+    }),
+    http.post('/api/logout', () => {
+        return HttpResponse.json({}, {
+            headers: {
+                'Set-Cookie': 'connect.sid=;HttpOnly;Path=/'
+            }
+        })
+    }),
+    http.post('/api/users', async () => {
+        console.log('회원가입');
+        // return HttpResponse.text(JSON.stringify('user_exists'), {
+        //     status: 403
+        // })
+        return HttpResponse.text(JSON.stringify('ok'), {
+            headers: {
+               'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/' 
+            }
+        })
+    })
+]
