d499eaf795d8b5d6cf8869edb7ad90966122b75b
김가영 <kimgayoung957@gmail.com>
2024-08-01 17:43:32 +0900

    search result add

---
+++ src/app/(afterLogin)/search/_component/SearchResult.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Post as IPost } from "@/model/Post";
+import { useQuery } from "@tanstack/react-query";
+import Post from "../../_component/Post";
+import { getSearchResult } from "../_lib/getSearchResult";
+
+type Props = {
+    searchParams: { q: string, f?: string, pf?: string }
+}
+export default function SearchResult({ searchParams }: Props) {
+    const { data } = useQuery<IPost[], object, IPost[], [_1: string, _2: string, Props['searchParams']]>({
+        queryKey: ["posts", "search", searchParams],
+        queryFn: getSearchResult,
+        staleTime: 60 * 1000,
+        gcTime: 300 * 1000,
+    });
+
+    console.log('search result data :::', data)
+
+    return data?.map((post) => (
+        <Post key={post.postId} post={post} />
+    ))
+
+}
